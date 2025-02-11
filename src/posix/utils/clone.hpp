7480def6ec4d41c7daa045d367383952123a9565
Marco Edoardo Santimaria <marcoedoardo.santimaria@unito.it>
2025-02-11 10:16:05 +0100

    Fixes

--- src/posix/utils/clone.hpp
+++ src/posix/utils/clone.hpp
@@ -43,7 +43,7 @@ inline void init_process(pid_t tid) {
 
     syscall_no_intercept_flag = true;
 
-    auto name = SHM_COMM_CHAN_NAME_RESP + std::to_string(tid);
+    auto name = std::string(SHM_COMM_CHAN_NAME_RESP) + std::to_string(tid);
     LOG("Allocating new circular buffer with name: %s", name.c_str());
     auto *p_buf_response =
         new CircularBuffer<capio_off64_t>(name, CAPIO_REQ_BUFF_CNT, sizeof(capio_off64_t));
